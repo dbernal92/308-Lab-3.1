22f222def5922b294b6746cfc337b7a6b0595ed9
Darien Bernal <darien.bernal@gmail.com>
2025-01-15 17:34:00 -0500

    Completed Part 2, added comments throughout code

--- index.js
+++ index.js
@@ -29,27 +29,10 @@ for (let f = 1; f <= 100; f++)
 // Create a loop that searches for the next prime number, starting at n and incrementing from there.
 // As soon as you find the prime number, log that number and exit the loop.
 
-// let n = 90;
-// for (let p = n; p <= 100; p++) {
-//     let isPrime = true;
-
-//     let nextPrime = p + 1;
-//     for (let d = 2; d < nextPrime; d++){
-
-//         if (nextPrime % d === 0) {
-//             isPrime = false;
-//         }
-//     }
-
-//     if (isPrime) {
-//         console.log(`Number: ${n}`)
-//         console.log(`Next prime number: ${nextPrime}`)
-//         break;
-//     }
-
-// }
-
+// User inputs a random number that is less than 100
 let n = 10;
+// The lopp is checking the next number to see whether or not it's prime
+// Create a variable to represent the following number (by adding 1 to the original)
 for (let p = n + 1; p <= 100; p++) {
     let isPrime = true;
 
@@ -80,19 +63,40 @@ for (let p = n + 1; p <= 100; p++) {
 
 const csvString ="Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232";
 
+// Create an array to store rows
 let rows = [];
+// Create variable with an empty string
 let currentRow = "";
 
+// Separate csvString into rows
 for (let i = 0; i < csvString.length; i++) {
-    if (csvString[i] === '\n') {
-        rows[rows.length] = currentRow;
-        currentRow = "";
+    if (csvString[i] === '\n') { // End of the row
+        rows[rows.length] = currentRow; // Puts the finished row in the array
+        currentRow = ""; // Beginning for new row
     } else {
-        currentRow += csvString[i];
+        currentRow += csvString[i]; // Each character goes into the variable one by one per loop
     }
-    rows[rows.length] = currentRow;
 }
+rows[rows.length] = currentRow; // Adds the last row, which doesn't need with the break
+console.log(rows)
 
+// Create an array to store headers
+let headers = [];
+// Create variable wirh an empty string
+let currentHeader = "";
+
+// Extract headers
+for (let i = 0; i < rows[0].length; i++) {
+    if (rows[0][i] === ",") { // End of the header
+        headers[headers.length] = currentHeader; // Puts the finished header in the array
+        currentHeader = ""; // Beginning for new header
+    } else {
+        currentHeader += rows[0][i];
+    }
+}
+headers[headers.length] = currentHeader;
+
+console.log(headers);
 
 
 // const rows = csvString.split("\n"); // Split by line breaks into rows
